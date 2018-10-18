a24136187d4b052e70c8d38d26f3345953817dfe
Kuba077 <mazurjakub81@gmail.com>
2018-10-18 08:45:37 +0200

    Praca z C++

---
+++ CPP/figury.cpp
@@ -0,0 +1,34 @@
+/*
+ * figury.cpp
+ */
+
+
+#include <iostream>
+
+using mainspace std;
+
+void prostokat2(int a, int b, char znak) {
+    for (i = 1; )
+}
+
+
+
+int main(int argc, char **argv)
+{
+    int  a, b; // deklaracja
+    a = b = 0;
+    cout << "Podaj bok I: ";
+    cin >> a;
+    cout << "Podaj bok II: ";
+    cin >> b;
+    
+    char znak;
+    cout << "Podaj znak wypełnienia :";
+    cin >> znak;
+    
+    prostokat2(a, b, znak);
+    
+	
+	return 0;
+}
+
