c10e2d522cd84326ac0db88bb2644a8e4737db00
Kuba077 <mazurjakub81@gmail.com>
2018-10-19 14:20:58 +0200

    Praca z C++ (Euklides)

--- Algorytmy/suma_cyfr.py
+++ Algorytmy/suma_cyfr.py
@@ -1,44 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  suma_cyfr.py
-#  
-#  Copyright 2018  <>
+#  suma-cyfr.py
 #  
 
-def sumuj_cyfry1(liczba):
+def suma1(liczba):
     suma = 0
     while liczba > 0:
         suma += liczba % 10
         liczba = int(liczba / 10)
     return suma
-
-def sumuj_cyfry2(liczba):
-    suma = 0
-    while liczba > 0:
-        suma += liczba % 10
-        liczba = int(liczba / 10)
-    return suma
-
-
-
-def main(args):
     
-    liczba = input("Podaj liczbę 2-cyfrową: ")
-    liczba = int(liczba)
+def suma2(liczba):
+    suma = 0
+    for cyfra in str(liczba):
+        suma += int(cyfra)
+    return suma
+    
+def main(args):
+    liczba = int(input("Podaj liczbę dwucyfrową: "))
     
     while liczba < 10:
-        liczba = input("Podaj liczbę 2-cyfrową: ")
-        liczba = int(liczba)
+        liczba = int(input("Podaj liczbę dwucyfrową: "))
     
-    
-    suma = 0
-    while liczba > 0:
-        suma += liczba % 10
-        liczba = int(liczba / 10)
-    
-    print("Suma:", suma)
-    
+    print("Suma: ", suma2(liczba))
+		
     return 0
 
 if __name__ == '__main__':
