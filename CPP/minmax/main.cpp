c330858a9de9e659467b134b56818609aee296d7
Kuba077 <mazurjakub81@gmail.com>
2018-10-25 08:47:00 +0200

    CPP codeblock

--- CPP/minmax/main.cpp
+++ CPP/minmax/main.cpp
@@ -5,14 +5,62 @@ using namespace std;
 void minmax1() {
     char dalej = 't';
     int liczba = 0;
-    int main = 0
-    while (dalej = 't') {
-        cout << "Podaj liczbę";
+    int min, max;
+    cout << "Podaj liczbę ";
+    cin >> liczba;
+    min = max = liczba;
+    while (dalej == 't') {
+        cout << "Podaj liczbę ";
+        cin >> liczba;
+        if (liczba < min)
+                min = liczba ;
+            cout << "Następna (t/n)? ";
+            cin >> dalej;
+    }   if (liczba > max)
+                max = liczba ;
+            cout << "Następna (t/n)? ";
+            cin >> dalej;
+    cout << "Najmniejsza: " << min << endl;
+    cout << "Największa: " << max << endl;
+}
+
+void wypelnij(int tab[], int roz) {
+    cout << "Podaj " << roz << "liczb: " << endl;
+    for(int i=0; i < roz; i++) {
+        cin >> tab[i];
+
     }
 }
 
+
+void drukuj(int tab[], int roz) {
+    for(int i=0; i < roz; i++) {
+        cout << tab[i] << " ";
+    }
+}
+
+
+void wypelnij_los(int tab[], int roz) {
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for(int i=0; i < roz; i++) {
+        tab[i] = rand() % 101;
+
+    }
+}
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int rozmiar = 10;
+    int tab[rozmiar];
+    //wypelnij(tab , rozmiar);
+
+    wypelnij_los(tab, rozmiar);
+
+    drukuj(tab, rozmiar);
+
+
+    //minmax1();
     return 0;
 }
