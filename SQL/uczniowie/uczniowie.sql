ccdfde52b26bfb7da1e95bdcaabcb4859c1562b7
Kuba077 <mazurjakub81@gmail.com>
2018-10-29 08:46:10 +0100

    SQL - kwerendy

--- SQL/uczniowie/uczniowie.sql
+++ SQL/uczniowie/uczniowie.sql
@@ -1,46 +1,46 @@
 DROP TABLE IF EXISTS uczniowie;
-
-CREATE TABLE uczniowie(
-id INTEGER PRIMARY KEY NOT NULL, 
-imie TEXT,
-nazwisko TEXT,
-plec BOOLEAN,
-id_klasa INTEGER NOT NULL,
-egz_hum NUMERIC NOT NULL DEFAULT 0,
-egz_mat NUMERIC NOT NULL DEFAULT 0,
-egz_jez NUMERIC NOT NULL DEFAULT 0,
-FOREIGN KEY (id_klasa) REFERENCES klasy (id)
-
-);
-
 DROP TABLE IF EXISTS klasy;
-
-CREATE TABLE klasy(
-id INTEGER NOT NULL,
-klasa TEXT,
-rok_naboru INTEGER,
-rok_matury INTEGER
-);
-
 DROP TABLE IF EXISTS przedmioty;
-
-CREATE TABLE przedmioty(
-id INTEGER NOT NULL,
-przedmiot TEXT(20),
-imie_naucz TEXT(15),
-nazwisko_naucz TEXT(20),
-plec_naucz BOOLEAN
-);
-
 DROP TABLE IF EXISTS oceny;
 
-CREATE TABLE oceny(
-id INTEGER NOT NULL,
-datad DATE,
-id_uczen INTEGER NOT NULL,
-id_przedmiot INTEGER NOT NULL,
-ocena DECIMAL,
-FOREIGN KEY (id_uczen) REFERENCES uczniowie(id),
-FOREIGN KEY (id_przedmiot) REFERENCES przedmioty(id)
- 
+CREATE TABLE uczniowie
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    imie TEXT,
+    nazwisko TEXT,
+    plec BOOLEAN,
+    id_klasa INTEGER NOT NULL,
+    egz_hum NUMERIC NOT NULL DEFAULT 0,
+    egz_mat NUMERIC NOT NULL DEFAULT 0,
+    egz_jez NUMERIC NOT NULL DEFAULT 0,
+    FOREIGN KEY (id_klasa) REFERENCES klasy(id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE klasy
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    klasa TEXT,
+    rok_naboru INTEGER,
+    rok_matury INTEGER
+);
+
+CREATE TABLE przedmioty
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    przedmiot TEXT,
+    imie_naucz TEXT,
+    nazwisko_naucz TEXT,
+    plec_naucz BOOLEAN
+);
+
+CREATE TABLE oceny
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    datad DATA,
+    id_uczen INTEGER NOT NULL,
+    id_przedmiot INTEGER NOT NULL,
+    ocena DECIMAL NOT NULL,
+    FOREIGN KEY (id_uczen) REFERENCES uczniowie(id)
+    FOREIGN KEY (id_przedmiot) REFERENCES przedmioty(id)
 );
