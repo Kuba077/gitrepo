8acfe5f47ac98af32ba4a20e20ec7aa1bf2ee162
Kuba077 <mazurjakub81@gmail.com>
2018-12-14 13:29:54 +0100

    Sortowanie w CPP

--- CPP/sortowanie.cpp
+++ CPP/sortowanie.cpp
@@ -7,52 +7,60 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-void wypelnij(int tab[], int roz) {
-    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
-    for(int i=0; i < roz; i++) {
-        //~cout << rand() << endl;
-        tab[i] = rand() % 101;
 
+void wypelnij(int tab[], int rozmiar) {
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for(int i=0; i < rozmiar; i++) {
+        // cout << rand() << endl;
+        tab[i] = rand() % 101;
+    }
+}
+
+void drukuj(int tab[], int rozmiar) {
+    for(int i=0; i < rozmiar; i++) {
+        cout << tab[i] << " ";
     }
     cout << endl;
 }
 
-
-void drukuj(int tab[], int roz) {
-    for(int i=0; i < roz; i++) {
-        cout << tab[i] << " ";
-    }
-}
-
-void zamien1(int a, int b) {
-    cout << a << " " << b << endl;
+void zamien1(int &a, int &b) {
+    // cout << a << " " << b << endl;
     int tmp = a;
     a = b;
     b = tmp;
-    cout << a << " " << b << endl; 
+    // cout << a << " " << b << endl;
+    }
 
+void zamien2(int tab[], int i) {
+    int tmp = tab[i];
+    tab[i] = tab[i+1];
+    tab[i+1] = tmp;
+    
 }
+
+
 void sort_bubble(int tab[], int n){
-    for(;;){
-        for(;;){
-            //~if (tab[i] > tab[i+1])
-                //~zamien1(tab[i], tab[i+1]);
+    for(int j = n - 1; j > 0; j--){
+       for(int i = 0; i < j; i++) {
+          if (tab[i] > tab[i + 1])
+            zamien2(tab, i);
+           }     
         }
     }
-}
 
 
 int main(int argc, char **argv)
 {
-	int roz = 20;
+    int roz = 20;
     int tab[roz];
-    wypelnij(tab, roz);
+	wypelnij(tab, roz);
     drukuj(tab, roz);
     cout << endl;
-    tab[0] = 7;
-    tab[1] = 5;
-    zamien1(tab[0], tab[i]);
-    cout << tab[0] << tab[i] << endl; 
-	return 0;
+    sort_bubble(tab, roz);
+    drukuj(tab, roz);
+    //~tab[0] = 7;
+    //~tab[1] = 5;
+    //~zamien1(tab[0], tab[1]);
+    //~cout << tab[0] << " " << tab[1];
+    return 0;
 }
-
