7b4ef6429e27716212af3b34b9cf286253a110c9
Kuba077 <mazurjakub81@gmail.com>
2019-01-30 10:35:15 +0100

    Nowy plik

---
+++ CPP/nieparzyste.cpp
@@ -0,0 +1,27 @@
+/*
+ * nieparzyste.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cout << "Podaj dowolną liczbę:";
+    cin >> n;
+    
+    for (int i = 1; i++){
+        if(i == n)
+            cout << n;
+        else
+        
+    }
+	
+	return 0;
+}
+
